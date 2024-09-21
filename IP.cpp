58f3a3cef62a551c7e564d0dd4b5798a0a7a0d3e
KerikK <kostiakedrik24@gmail.com>
2024-09-21 18:01:40 +0300

    Add files via upload

---
+++ IP.cpp
@@ -0,0 +1,82 @@
+﻿//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Enter sentence:\n";
+//    string text;
+//    getline(cin, text);
+//
+//
+//    transform(text.begin(), text.end(), text.begin(), ::tolower);
+//    int c;
+//    cout << "Enter setntence:\n";
+//    cin >> c;
+//    c--;
+//
+//    if (c >= 0 && c < text.size()) {
+//        text.erase(c, 1);
+//        cout << endl << text;
+//    }
+//    else {
+//        cout << endl << "eror";
+//    }
+
+
+
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Enter sentence:\n";
+//    string text;
+//    getline(cin, text);
+//
+//
+//    transform(text.begin(), text.end(), text.begin(), ::tolower);
+//    string line;
+//    getline(cin, line);
+//
+//    int c;
+//    cout << "Enter setntence:\n";
+//    cin >> c;
+//    c--;
+//
+//    if (c >= 0 && c < text.size()) {
+//       // text.erase(c, 1);
+//        text.insert(c, line);
+//        cout << endl << text;
+//    }
+//    else {
+//        cout << endl << "eror";
+//    }
+//}
+
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    string ip;
+    getline(cin, ip);
+    long suma = 0;
+    for (char s : ip) {
+        if (s == '.') suma++;
+    }
+    if (suma > 3)
+        cout << "Некоректний адрес";
+}
